31cd81303ccf2fe33b4e59554e915e707926a308
Tom Fan <119341778+TomFanHM@users.noreply.github.com>
2024-08-19 19:08:03 +0800

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import { readFileSync } from "fs"
+import path from "path"
+import { defineConfig } from "vite"
+import dts from "vite-plugin-dts"
+import tsconfigPaths from "vite-tsconfig-paths"
+
+const packageJson = JSON.parse(readFileSync("./package.json", { encoding: "utf-8" }))
+
+const globals = {
+  ...(packageJson.dependencies || {}),
+}
+
+function resolve(str: string) {
+  return path.resolve(__dirname, str)
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  mode: "production",
+  build: {
+    lib: {
+      entry: resolve("./src/index.ts"),
+      fileName: (format, entryName) => `${entryName}.js`,
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: [...Object.keys(globals)],
+      input: {
+        index: resolve("./src/index.ts"),
+      },
+    },
+  },
+  plugins: [
+    dts({
+      exclude: ["**/*.test.ts", "demo/**/*"],
+    }),
+    tsconfigPaths(),
+  ],
+})
